653fa2c6c24d68bdba915122365caffd8244c3cc
Z-ajamy <abdorahman0283@gmail.com>
2025-09-21 00:34:25 +0300

    DBStorage - Review

--- models/place.py
+++ models/place.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, INTEGER, FLOAT
 from sqlalchemy.orm import relationship
+from os import getenv
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -22,3 +24,18 @@ class Place(BaseModel, Base):
 
     cities = relationship("City", back_populates="places")
     user = relationship("User", back_populates="places")
+
+    if getenv("HBNB_TYPE_STORAGE") == "file":
+        @properties
+        def reviews(self):
+            from models import storage
+            from models.review import Review 
+            review_list = []
+            dic_of_all_reviews = storage.all(Review)
+            for city in dic_of_all_reviews.values():
+                if self.id == review.place_id:
+                    review_list.append(city)
+            return review_list
+    else:
+        reviews = relationship("Review", back_populates="place", cascade="all, delete, delete-orphan")
+
