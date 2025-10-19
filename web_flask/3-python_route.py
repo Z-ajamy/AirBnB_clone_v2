958f159b9eaa164091892109f3af31fdf41af7b2
Z-ajamy <abdorahman0283@gmail.com>
2025-10-19 22:34:39 +0300

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,16 +27,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
-
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def c(text="is cool"):
-    """Displays 'Python' followed by the value of <text>.
+    """Displays 'C' followed by the value of <text>.
 
     Replaces any underscores in <text> with slashes.
     """
@@ -44,5 +35,16 @@ def c(text="is cool"):
     return "C {}".format(text)
 
 
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
